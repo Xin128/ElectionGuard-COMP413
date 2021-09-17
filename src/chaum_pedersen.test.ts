088362afde035f65eb691245b864881e88528e77
Xin <xh23@rice.edu>
2021-09-17 15:05:07 -0500

    add tests

--- src/chaum_pedersen.test.ts
+++ src/chaum_pedersen.test.ts
@@ -26,8 +26,62 @@ describe("TestDisjunctiveChaumPedersen", () => {
         )
         expect(proof1.is_valid(message1, keypair.public_key, ONE_MOD_Q)).toBe(true);
         expect(proof1bad.is_valid(message1, keypair.public_key, ONE_MOD_Q)).toBe(false);
-
-        // expect(1).toBe(1)
     });
+
+
+    test('test_djcp_proof_invalid_inputs', () => {
+        // to push up our coverage
+        let keypair:ElGamalKeyPair = elgamal_keypair_from_secret(TWO_MOD_Q);
+        let nonce = ONE_MOD_Q
+        let seed = TWO_MOD_Q
+        let message0:ElGamalCiphertext = get_optional(elgamal_encrypt(0, nonce, keypair.public_key))
+        // reason for throwError: last signature (plaintext for chaum pedersen is 3: should be 0 or 1)
+        expect(make_disjunctive_chaum_pedersen(message0,nonce, keypair.public_key,seed,3)).toThrowError();
+    });
+
+    // Note: this test cannot be performed until electionguardtest folder is refactored. 
+    // the method elgamal_keypairs() generates an arbitrary elgamal secret/public keypair
+    test('test_djcp_proof_zero', () => {
+        // to push up our coverage
+        let keypair:ElGamalKeyPair = elgamal_keypairs();
+        let nonce: ElementModQ = elements_mod_q_no_zero();
+        let seed:ElementModQ = elements_mod_q();
+        let message:ElGamalCiphertext = get_optional(elgamal_encrypt(0, nonce, keypair.public_key))
+
+        let proof:DisjunctiveChaumPedersenProof = make_disjunctive_chaum_pedersen_zero(
+            message, nonce, keypair.public_key, ONE_MOD_Q, seed
+        );
+        let proof_bad:DisjunctiveChaumPedersenProof = make_disjunctive_chaum_pedersen_one(
+            message, nonce, keypair.public_key, ONE_MOD_Q, seed
+        );
+        expect(proof.is_valid(message, keypair.public_key, ONE_MOD_Q)).toBe(true);
+        expect(proof_bad.is_valid(message, keypair.public_key, ONE_MOD_Q)).toBe(false);
+
+       
+    });
+
+
+        // Note: this test cannot be performed until electionguardtest folder is refactored. 
+    // the method elgamal_keypairs() generates an arbitrary elgamal secret/public keypair
+    test('test_djcp_proof_one', () => {
+        // to push up our coverage
+        let keypair:ElGamalKeyPair = elgamal_keypairs();
+        let nonce: ElementModQ = elements_mod_q_no_zero();
+        let seed:ElementModQ = elements_mod_q();
+        let message:ElGamalCiphertext = get_optional(elgamal_encrypt(1, nonce, keypair.public_key))
+
+        let proof:DisjunctiveChaumPedersenProof = make_disjunctive_chaum_pedersen_one(
+            message, nonce, keypair.public_key, ONE_MOD_Q, seed
+        );
+        let proof_bad:DisjunctiveChaumPedersenProof = make_disjunctive_chaum_pedersen_zero(
+            message, nonce, keypair.public_key, ONE_MOD_Q, seed
+        );
+        expect(proof.is_valid(message, keypair.public_key, ONE_MOD_Q)).toBe(true);
+        expect(proof_bad.is_valid(message, keypair.public_key, ONE_MOD_Q)).toBe(false);
+
+       
+    });
+
+
 });
 
