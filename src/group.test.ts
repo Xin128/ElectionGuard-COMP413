2064c658a743ffc776f543856e28d8f588801d4a
Yanyu Zhong <yz133@rice.edu>
2021-09-18 12:46:14 -0500

    Finished group.test.ts

--- src/group.test.ts
+++ src/group.test.ts
@@ -31,11 +31,10 @@ import {
 } from './utils';
 
 import {
-    elementsModQ
-    // elements_mod_p_no_zero,
-    // elements_mod_p,
-    // elements_mod_q,
-    // elements_mod_q_no_zero,
+    elementsModQ,
+    elementsModP,
+    elementsModPNoZero,
+    elementsModQNoZero
 } from './groupUtils';
 
 describe("TestEquality", () => {
@@ -64,89 +63,150 @@ describe("TestEquality", () => {
 
 describe("TestModularArithmetic", () => {
     test('testAddQ', () => {
-
+        const q: ElementModQ = elementsModQ();
+        const asInt: ElementModQ = addQ(q, 1);
+        const asElem: ElementModQ = addQ(q, new ElementModQ(BigInt(1)));
+        expect(asInt.equals(asElem)).toBe(true);
     });
 
     test('testAPlusBCQ', () => {
-
+        const q: ElementModQ = elementsModQ();
+        const asInt: ElementModQ = aPlusBCQ(q, 1, 1);
+        const asElem: ElementModQ = aPlusBCQ(q, new ElementModQ(BigInt(1)), new ElementModQ(BigInt(1)));
+        expect(asInt.equals(asElem)).toBe(true);
     });
 
     test('testAMinusBQ', () => {
-
+        const q: ElementModQ = elementsModQ();
+        const asInt: ElementModQ = aMinusBQ(q, 1);
+        const asElem: ElementModQ = aMinusBQ(q, new ElementModQ(BigInt(1)));
+        expect(asInt.equals(asElem)).toBe(true);
     });
 
     test('testDivQ', () => {
-
+        const q: ElementModQ = elementsModQ();
+        const asInt: ElementModQ = divQ(q, 1);
+        const asElem: ElementModQ = divQ(q, new ElementModQ(BigInt(1)));
+        expect(asInt.equals(asElem)).toBe(true);
     });
 
     test('testDivP', () => {
-
+        const p: ElementModP = elementsModP();
+        const asInt: ElementModP = divP(p, 1);
+        const asElem: ElementModP = divP(p, new ElementModP(BigInt(1)));
+        expect(asInt.equals(asElem)).toBe(true);
     });
 
     test('testNoMultInvOfZero', () => {
-
+        // TODO: may need to know which exception to throw
+        expect(() => multInvP(ZERO_MOD_P)).toThrow(Error);
     });
 
     test('testMultInverses', () => {
-
+        const elem: ElementModP = elementsModPNoZero();
+        const inv: ElementModP = multInvP(elem);
+        expect(multP(elem, inv).equals(ONE_MOD_P)).toBe(true);
     });
 
     test('testMultIdentity', () => {
-
+        const elem: ElementModP = elementsModP();
+        expect(elem.equals(multP(elem))).toBe(true);
     });
 
     test('testMultNoArgs', () => {
-
+        expect(ONE_MOD_P.equals(multP())).toBe(true);
     });
 
     test('testAddNoArgs', () => {
-
+        expect(ZERO_MOD_Q.equals(addQ())).toBe(true);
     });
 
     test('testPropertiesForConstants', () => {
-
+        expect(G !== 1).toBe(true);
+        expect((R * Q) % P).toEqual(P - 1);
+        expect(Q).toBeLessThan(P);
+        expect(G).toBeLessThan(P);
+        expect(R).toBeLessThan(P);
     });
 
     test('testSimplePowers', () => {
-
+        const gp: ElementModP = intToP(G);
+        expect(gp.equals(gPowP(ONE_MOD_Q))).toBe(true);
+        expect(ONE_MOD_P.equals(gPowP(ZERO_MOD_Q)));
     });
 
     test('testInBoundsQ', () => {
-
+        const q: ElementModQ = elementsModQ();
+        expect(q.isInBounds()).toBe(true);
+        const tooBig: number = q.toInt() + Q;
+        const tooSmall: number = q.toInt() - Q;
+        expect(intToQUnchecked(tooBig).isInBounds()).toBe(false);
+        expect(intToQUnchecked(tooSmall).isInBounds()).toBe(false);
+        expect(intToQ(tooBig)).toEqual(null);
+        expect(intToQ(tooSmall)).toEqual(null);
     });
 
     test('testInBoundsP', () => {
-
+        const p: ElementModP = elementsModP();
+        expect(p.isInBounds()).toBe(true);
+        const tooBig: number = p.toInt() + P;
+        const tooSmall: number = p.toInt() - P;
+        expect(intToPUnchecked(tooBig).isInBounds()).toBe(false);
+        expect(intToPUnchecked(tooSmall).isInBounds()).toBe(false);
+        expect(intToP(tooBig)).toEqual(null);
+        expect(intToP(tooSmall)).toEqual(null);
     });
 
     test('testInBoundsQNoZero', () => {
-
+        const q: ElementModQ = elementsModQNoZero();
+        expect(q.isInBoundsNoZero()).toBe(true);
+        expect(ZERO_MOD_Q.isInBoundsNoZero()).toBe(false);
+        expect(intToQUnchecked(q.toInt() + Q).isInBoundsNoZero()).toBe(false);
+        expect(intToQUnchecked(q.toInt() - Q).isInBoundsNoZero()).toBe(false);
     });
 
     test('testInBoundsPNoZero', () => {
-
+        const p: ElementModP = elementsModPNoZero();
+        expect(p.isInBoundsNoZero()).toBe(true);
+        expect(ZERO_MOD_P.isInBoundsNoZero()).toBe(false);
+        expect(intToPUnchecked(p.toInt() + P).isInBoundsNoZero()).toBe(false);
+        expect(intToPUnchecked(p.toInt() - P).isInBoundsNoZero()).toBe(false);
     });
 
     test('testLargeValuesRejectedByInToQ', () => {
-
+        const q: ElementModQ = elementsModQ();
+        const oversize: number = q.toInt() + Q;
+        expect(intToQ(oversize)).toEqual(null);
     });
 });
 
 describe("TestOptionalFunctions", () => {
     test('testUnwrap', () => {
-
+        const good: number | null = 3;
+        const bad: number | null = null;
+        expect(get_optional(good)).toEqual(3);
+        expect(() => get_optional(bad)).toThrow(Error);
     });
 
     test('testMatch', () => {
-
+        const good: number | null = 3;
+        const bad: number | null = null;
+        expect(match_optional(good, () => 1, (x) => x + 2)).toEqual(5);
+        expect(match_optional(bad, () => 1, (x) => x + 2)).toEqual(1);
     });
 
     test('testGetOrElse', () => {
-
+        const good: number | null = 3;
+        const bad: number | null = null;
+        expect(get_or_else_optional(good, 5)).toEqual(3);
+        expect(get_or_else_optional(bad, 5)).toEqual(5);
     });
 
     test('testFlatMap', () => {
-
+        const good: number | null = 3;
+        const bad: number | null = null;
+        expect(get_optional(flatmap_optional(good, (x) => x + 2))).toEqual(5);
+        expect(flatmap_optional(bad, (x) => x + 2)).toBeNull();
     });
 });
 
--- src/groupUtils.ts
+++ src/groupUtils.ts
@@ -1,7 +1,19 @@
 // Helper functions for group.test.ts
-import {ElementModQ, ElementModP, Q, intToQUnchecked} from './group';
+import {ElementModQ, ElementModP, Q, P, intToQUnchecked, intToPUnchecked} from './group';
 const elementsModQ: () => ElementModQ = () => {
     return intToQUnchecked(getRandomIntInclusive(0, Q - 1));
+}
+
+const elementsModQNoZero: () => ElementModQ = () => {
+    return intToQUnchecked(getRandomIntInclusive(1, Q - 1));
+}
+
+const elementsModP: () => ElementModP = () => {
+    return intToPUnchecked(getRandomIntInclusive(0, P - 1));
+}
+
+const elementsModPNoZero: () => ElementModP = () => {
+    return intToPUnchecked(getRandomIntInclusive(1, P - 1));
 }
 
 const getRandomIntInclusive: (min: number, max: number) => number = (min, max) => {
@@ -10,4 +22,4 @@ const getRandomIntInclusive: (min: number, max: number) => number = (min, max) =
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
 
-export {elementsModQ};
+export {elementsModQ, elementsModQNoZero, elementsModP, elementsModPNoZero};
