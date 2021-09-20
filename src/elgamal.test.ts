64ec5720d79c2bb1f7c6f9f4b9d41d61d3db3d77
Xin <xh23@rice.edu>
2021-09-20 14:20:17 -0500

    add elgamal test

--- src/elgamal.test.ts
+++ src/elgamal.test.ts
@@ -18,34 +18,27 @@ import {ElGamalKeyPair,
     elgamal_add,
     elgamal_keypair_from_secret,
     elgamal_keypair_random,} from './elgamal'
+import exp from 'constants';
 
 describe("TestElgamal", () => {
     test('test_simple_elgamal_encryption_decryption', () => {
         const nonce = ONE_MOD_Q;
         const secret_key = TWO_MOD_Q
-        const keypair:ElGamalKeyPair|undefined = get_optional(elgamal_keypair_from_secret(secret_key));
-        const public_key = keypair.public_key;
-
+        const keypair:ElGamalKeyPair|undefined = elgamal_keypair_from_secret(secret_key);
+        const public_key:number = keypair.public_key;
         expect(public_key.to_int()).toBeLessThan(P);
-        
+        expect(Math.pow(public_key.to_int(), nonce.to_int())).toBe(P);
+
         
         const elem:ElementModP = g_pow_p(ZERO_MOD_Q);
         expect(elem.equals(ONE_MOD_P)).toBe(true); // g^0 == 1
 
-        const ciphertext:ElGamalCiphertext|undefined = get_optional(elgamal_encrypt(0, nonce, keypair.public_key));
+        const ciphertext:ElGamalCiphertext|undefined = elgamal_encrypt(0, nonce, keypair.public_key);
         expect(G).toBe(ciphertext.pad.to_int());
-       expect(
-            pow(ciphertext.pad.to_int(), secret_key.to_int(), P),
-            pow(public_key.to_int(), nonce.to_int(), P),
-        )
-        self.assertEqual(
-            ciphertext.data.to_int(),
-            pow(public_key.to_int(), nonce.to_int(), P),
-        )
+        expect(Math.pow(ciphertext.pad.to_int(), secret_key.to_int())).toBe(P));
 
-        plaintext = ciphertext.decrypt(keypair.secret_key)
+        const plaintext:number = ciphertext.decrypt(keypair.secret_key);
 
-        self.assertEqual(0, plaintext)
+        expect(plaintext).toBe(0);
     });
-
-});
+    });
