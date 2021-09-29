65cdd9da490d659c473f621cad7eb22c10e608fc
Xin <xh23@rice.edu>
2021-09-29 12:53:27 -0500

    only one test failed:test_elgamal_keypair_produces_valid_residue

--- src/elgamal.test.ts
+++ src/elgamal.test.ts
@@ -1,21 +1,22 @@
 import {
-  // ElementModQ,
+  ElementModQ,
     ElementModP,
     g_pow_p,
     G,
     P,
-    // Q,
+    Q,
     ZERO_MOD_Q,
     TWO_MOD_Q,
     ONE_MOD_Q,
     ONE_MOD_P,} from './group';
+import { elements_mod_q_no_zero} from './groupUtils';
 // import {hash_elems} from './hash';
 import {ElGamalKeyPair,
     ElGamalCiphertext,
     elgamal_encrypt,
-    // elgamal_add,
+    elgamal_add,
     elgamal_keypair_from_secret,
-    // elgamal_keypair_random
+    elgamal_keypair_random
 } from './elgamal';
 import {get_optional} from "./utils";
 // import exp from 'constants';
@@ -40,5 +41,108 @@ describe("TestElgamal", () => {
       // const plaintext:number = ciphertext.decrypt(keypair.secret_key);
         //
         // expect(plaintext).toBe(0);
+    });
+
+    test('test_elgamal_encrypt_requires_nonzero_nonce', () => {
+        const max = 100;
+        const min = 0;
+        const message = BigInt(Math.floor(Math.random() * (max - min + 1) + min));
+        const keypair:ElGamalKeyPair|null = elgamal_keypair_from_secret(elements_mod_q_no_zero());
+        expect(elgamal_encrypt(message, ZERO_MOD_Q, keypair!.public_key)).toBe(null);
+    });
+
+    test('test_elgamal_keypair_from_secret_requires_key_greater_than_one', () => {
+        expect(elgamal_keypair_from_secret(ZERO_MOD_Q)).toBe(null);
+        expect(elgamal_keypair_from_secret(ONE_MOD_Q)).toBe(null);
+
+    });
+
+    test('test_elgamal_encryption_decryption_inverses', () => {
+        const max = 100;
+        const min = 0;
+        const message = BigInt(Math.floor(Math.random() * (max - min + 1) + min));
+        const keypair:ElGamalKeyPair|null = elgamal_keypair_from_secret(elements_mod_q_no_zero());
+        const nonce:ElementModQ|null = elements_mod_q_no_zero();
+
+        // to avoid non-used issue
+        console.log(message,keypair,nonce);
+
+        // const plaintext:number = ciphertext.decrypt(keypair.secret_key);
+        // const ciphertext = elgamal_encrypt(message, nonce, keypair.public_key)
+        // expect(plaintext).toBe(message);
+    });
+
+    test('test_elgamal_encryption_decryption_with_known_nonce_inverses', () => {
+        const max = 100;
+        const min = 0;
+        const message = BigInt(Math.floor(Math.random() * (max - min + 1) + min));
+        const keypair:ElGamalKeyPair|null = elgamal_keypair_from_secret(elements_mod_q_no_zero());
+        const nonce:ElementModQ|null = elements_mod_q_no_zero();
+
+        // to avoid non-used issue
+        console.log(message,keypair,nonce);
+        
+        // const ciphertext = elgamal_encrypt(message, nonce, keypair!.public_key)
+        // const plaintext:number = ciphertext.decrypt_known_nonce(keypair!.secret_key);
+        // expect(plaintext).toBe(message);
+    });
+
+    test('test_elgamal_generated_keypairs_are_within_range', () => {
+        const keypair:ElGamalKeyPair|null = elgamal_keypair_from_secret(elements_mod_q_no_zero());
+        expect(keypair!.public_key.to_int()).toBeLessThan(P);
+        expect(keypair!.secret_key.to_int()).toBeLessThan(Q);
+        expect(g_pow_p(keypair!.secret_key).equals(keypair!.public_key)).toBe(true);
+    });
+
+    test('test_elgamal_add_homomorphic_accumulation_decrypts_successfully', () => {
+        const r1 = elements_mod_q_no_zero();
+        const r2 = elements_mod_q_no_zero();
+        const max = 100;
+        const min = 0;
+        const m1 = BigInt(Math.floor(Math.random() * (max - min + 1) + min));
+        const m2 = BigInt(Math.floor(Math.random() * (max - min + 1) + min));
+        const keypair:ElGamalKeyPair|null = elgamal_keypair_from_secret(elements_mod_q_no_zero());
+
+        const c1 =  elgamal_encrypt(m1, r1, keypair!.public_key)
+        const c2 =  elgamal_encrypt(m2, r2, keypair!.public_key)
+        const c_sum = elgamal_add(c1!, c2!);
+
+          // to avoid non-used issue
+          console.log(c_sum);
+        // const total = c_sum.decrypt(keypair.secret_key)
+        // expect(total).toBe(m1 + m2);
+    });
+
+
+    test('test_elgamal_add_requires_args', () => {
+        //console.assert does not throw an AssertionError (except in Node.js), meaning that this method is incompatible with most testing frameworks and that code execution will not break on a failed assertion.
+        // TODO: add manual defined error in elgamal.
+        expect(elgamal_add()).toBe(undefined);
+    });
+    
+    
+    test('test_elgamal_keypair_produces_valid_residue', () => {
+        let e = elements_mod_q_no_zero();
+        if (e == ONE_MOD_Q) {
+            e = TWO_MOD_Q;
+        }
+        const keypair:ElGamalKeyPair|null = elgamal_keypair_from_secret(e);
+
+        expect(keypair!.public_key.is_valid_residue()).toBe(true);
+    });
+
+
+    test('test_elgamal_keypair_random', () => {
+        let e = elements_mod_q_no_zero();
+        if (e == ONE_MOD_Q) {
+            e = TWO_MOD_Q;
+        }
+        const random_keypair = elgamal_keypair_random();
+        const random_keypair_two = elgamal_keypair_random();
+        expect(random_keypair).toBeDefined();
+        expect(random_keypair.public_key).toBeDefined();
+        expect(random_keypair.secret_key).toBeDefined();
+        expect(random_keypair != random_keypair_two).toBeTruthy();
+
     });
 });
