dc5806eb5898f0f354cc00f0f36f48f628463014
wwq <wuwenqin200613@126.com>
2021-10-04 22:45:33 -0500

    elgamal decrypt test passed

--- src/elgamal.test.ts
+++ src/elgamal.test.ts
@@ -38,9 +38,9 @@ describe("TestElgamal", () => {
         expect(get_optional(ciphertext).pad.to_int() ** secret_key.to_int() % P).toBe(public_key.to_int() ** nonce.to_int() % P);
         expect(get_optional(ciphertext).data.to_int()).toBe(public_key.to_int() ** nonce.to_int() % P);
 
-      // const plaintext:number = ciphertext.decrypt(keypair.secret_key);
-        //
-        // expect(plaintext).toBe(0);
+        const plaintext:bigint = get_optional(ciphertext).decrypt(get_optional(keypair).secret_key);
+
+        expect(plaintext).toBe(0n);
     });
 
     test('test_elgamal_encrypt_requires_nonzero_nonce', () => {
@@ -65,11 +65,11 @@ describe("TestElgamal", () => {
         const nonce:ElementModQ|null = elements_mod_q_no_zero();
 
         // to avoid non-used issue
-        console.log(message,keypair,nonce);
+        // console.log(message,keypair,nonce);
 
-        // const plaintext:number = ciphertext.decrypt(keypair.secret_key);
-        // const ciphertext = elgamal_encrypt(message, nonce, keypair.public_key)
-        // expect(plaintext).toBe(message);
+        const ciphertext = elgamal_encrypt(message, nonce, get_optional(keypair).public_key)
+        const plaintext:bigint = get_optional(ciphertext).decrypt(get_optional(keypair).secret_key);
+        expect(plaintext).toBe(message);
     });
 
     test('test_elgamal_encryption_decryption_with_known_nonce_inverses', () => {
@@ -80,11 +80,11 @@ describe("TestElgamal", () => {
         const nonce:ElementModQ|null = elements_mod_q_no_zero();
 
         // to avoid non-used issue
-        console.log(message,keypair,nonce);
+        // console.log(message,keypair,nonce);
 
-        // const ciphertext = elgamal_encrypt(message, nonce, keypair!.public_key)
-        // const plaintext:number = ciphertext.decrypt_known_nonce(keypair!.secret_key);
-        // expect(plaintext).toBe(message);
+        const ciphertext = elgamal_encrypt(message, nonce, get_optional(keypair).public_key)
+        const plaintext:bigint = get_optional(ciphertext).decrypt_known_nonce(get_optional(keypair).public_key, nonce);
+        expect(plaintext).toBe(message);
     });
 
     test('test_elgamal_generated_keypairs_are_within_range', () => {
@@ -108,9 +108,9 @@ describe("TestElgamal", () => {
         const c_sum = elgamal_add(c1!, c2!);
 
           // to avoid non-used issue
-          console.log(c_sum);
-        // const total = c_sum.decrypt(keypair.secret_key)
-        // expect(total).toBe(m1 + m2);
+          // console.log(c_sum);
+        const total = c_sum.decrypt(get_optional(keypair).secret_key);
+        expect(total).toBe(m1 + m2);
     });
 
 
