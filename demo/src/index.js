9fc5de4447b92b5fc1f4bb007e85c5ccaf12b5f6
Xin <xh23@rice.edu>
2021-10-05 10:54:11 -0500

    front end intraction commited

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,5 +1,3 @@
-import "./styles.css";
-
 document.getElementById("myBtn").addEventListener("click", function () {
   document.getElementById("output").innerHTML =
     "4E88B37B6887A8AD04885850A9E3F33A267E8907F16446506B3566F2B1177819";
