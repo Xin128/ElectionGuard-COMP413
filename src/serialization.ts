6463c320b1cc36eb9b59266575adeb71f3117a2a
wwq <wuwenqin200613@126.com>
2021-10-25 10:52:17 -0500

    serialization big bumber handeling

--- src/serialization.ts
+++ src/serialization.ts
@@ -1,5 +1,6 @@
 import {CiphertextBallot} from "./simple_election_data";
 import { plainToClass } from "class-transformer";
+import {get_optional} from "./utils";
 
 const json_string = "{\n" +
   "    \"object_id\": \"some-external-id-string-123\",\n" +
@@ -17,17 +18,17 @@ const json_string = "{\n" +
   "                    \"sequence_order\": 0,\n" +
   "                    \"description_hash\": 11775,\n" +
   "                    \"ciphertext\": {\n" +
-  "                        \"pad\": 12134001671678771851,\n" +
-  "                        \"data\": 1502770863777469358\n" +
+  "                        \"pad\": \"12134001671678771851\",\n" +
+  "                        \"data\": \"1502770863777469358\"\n" +
   "                    },\n" +
   "                    \"crypto_hash\": 39529,\n" +
   "                    \"is_placeholder_selection\": false,\n" +
   "                    \"nonce\": 50246,\n" +
   "                    \"proof\": {\n" +
-  "                        \"proof_zero_pad\": 16923428534602348243,\n" +
-  "                        \"proof_zero_data\": 17570393965625676549,\n" +
-  "                        \"proof_one_pad\": 10341002050983186017,\n" +
-  "                        \"proof_one_data\": 17215719614163014940,\n" +
+  "                        \"proof_zero_pad\": \"16923428534602348243\",\n" +
+  "                        \"proof_zero_data\": \"17570393965625676549\",\n" +
+  "                        \"proof_one_pad\": \"10341002050983186017\",\n" +
+  "                        \"proof_one_data\": \"17215719614163014940\",\n" +
   "                        \"proof_zero_challenge\": 4951,\n" +
   "                        \"proof_one_challenge\": 37371,\n" +
   "                        \"challenge\": 42322,\n" +
@@ -42,17 +43,17 @@ const json_string = "{\n" +
   "                    \"sequence_order\": 1,\n" +
   "                    \"description_hash\": 51671,\n" +
   "                    \"ciphertext\": {\n" +
-  "                        \"pad\": 3917837127134651645,\n" +
-  "                        \"data\": 5314367008433655299\n" +
+  "                        \"pad\": \"3917837127134651645\",\n" +
+  "                        \"data\": \"5314367008433655299\"\n" +
   "                    },\n" +
   "                    \"crypto_hash\": 8417,\n" +
   "                    \"is_placeholder_selection\": false,\n" +
   "                    \"nonce\": 22491,\n" +
   "                    \"proof\": {\n" +
-  "                        \"proof_zero_pad\": 13136678010011016740,\n" +
-  "                        \"proof_zero_data\": 2165135364934198121,\n" +
-  "                        \"proof_one_pad\": 3922526953898330832,\n" +
-  "                        \"proof_one_data\": 1255969695179565188,\n" +
+  "                        \"proof_zero_pad\": \"13136678010011016740\",\n" +
+  "                        \"proof_zero_data\": \"2165135364934198121\",\n" +
+  "                        \"proof_one_pad\": \"3922526953898330832\",\n" +
+  "                        \"proof_one_data\": \"1255969695179565188\",\n" +
   "                        \"proof_zero_challenge\": 61959,\n" +
   "                        \"proof_one_challenge\": 54277,\n" +
   "                        \"challenge\": 50715,\n" +
@@ -67,17 +68,17 @@ const json_string = "{\n" +
   "                    \"sequence_order\": 2,\n" +
   "                    \"description_hash\": 1054,\n" +
   "                    \"ciphertext\": {\n" +
-  "                        \"pad\": 255844785366588188,\n" +
-  "                        \"data\": 8583070084640768030\n" +
+  "                        \"pad\": \"255844785366588188\",\n" +
+  "                        \"data\": \"8583070084640768030\"\n" +
   "                    },\n" +
   "                    \"crypto_hash\": 43225,\n" +
   "                    \"is_placeholder_selection\": false,\n" +
   "                    \"nonce\": 31347,\n" +
   "                    \"proof\": {\n" +
-  "                        \"proof_zero_pad\": 10235258317480349978,\n" +
-  "                        \"proof_zero_data\": 7256985595115661952,\n" +
-  "                        \"proof_one_pad\": 10175758013699893708,\n" +
-  "                        \"proof_one_data\": 14720976612885448488,\n" +
+  "                        \"proof_zero_pad\": \"10235258317480349978\",\n" +
+  "                        \"proof_zero_data\": \"7256985595115661952\",\n" +
+  "                        \"proof_one_pad\": \"10175758013699893708\",\n" +
+  "                        \"proof_one_data\": \"14720976612885448488\",\n" +
   "                        \"proof_zero_challenge\": 32445,\n" +
   "                        \"proof_one_challenge\": 20055,\n" +
   "                        \"challenge\": 52500,\n" +
@@ -92,17 +93,17 @@ const json_string = "{\n" +
   "                    \"sequence_order\": 3,\n" +
   "                    \"description_hash\": 50863,\n" +
   "                    \"ciphertext\": {\n" +
-  "                        \"pad\": 9299912383351265113,\n" +
-  "                        \"data\": 212954059271314761\n" +
+  "                        \"pad\": \"9299912383351265113\",\n" +
+  "                        \"data\": \"212954059271314761\"\n" +
   "                    },\n" +
   "                    \"crypto_hash\": 60333,\n" +
   "                    \"is_placeholder_selection\": false,\n" +
   "                    \"nonce\": 60200,\n" +
   "                    \"proof\": {\n" +
-  "                        \"proof_zero_pad\": 15826453124332148024,\n" +
-  "                        \"proof_zero_data\": 6585726058992680212,\n" +
-  "                        \"proof_one_pad\": 1457762078489557579,\n" +
-  "                        \"proof_one_data\": 18230008489562257330,\n" +
+  "                        \"proof_zero_pad\": \"15826453124332148024\",\n" +
+  "                        \"proof_zero_data\": \"6585726058992680212\",\n" +
+  "                        \"proof_one_pad\": \"1457762078489557579\",\n" +
+  "                        \"proof_one_data\": \"18230008489562257330\",\n" +
   "                        \"proof_zero_challenge\": 11400,\n" +
   "                        \"proof_one_challenge\": 29068,\n" +
   "                        \"challenge\": 40468,\n" +
@@ -117,17 +118,17 @@ const json_string = "{\n" +
   "                    \"sequence_order\": 4,\n" +
   "                    \"description_hash\": 41361,\n" +
   "                    \"ciphertext\": {\n" +
-  "                        \"pad\": 11224162101144585914,\n" +
-  "                        \"data\": 1613948550166977864\n" +
+  "                        \"pad\": \"11224162101144585914\",\n" +
+  "                        \"data\": \"1613948550166977864\"\n" +
   "                    },\n" +
   "                    \"crypto_hash\": 60598,\n" +
   "                    \"is_placeholder_selection\": true,\n" +
   "                    \"nonce\": 52438,\n" +
   "                    \"proof\": {\n" +
-  "                        \"proof_zero_pad\": 12293112453131972810,\n" +
-  "                        \"proof_zero_data\": 17487731825999790825,\n" +
-  "                        \"proof_one_pad\": 13776221093905079620,\n" +
-  "                        \"proof_one_data\": 4713734307765350282,\n" +
+  "                        \"proof_zero_pad\": \"12293112453131972810\",\n" +
+  "                        \"proof_zero_data\": \"17487731825999790825\",\n" +
+  "                        \"proof_one_pad\": \"13776221093905079620\",\n" +
+  "                        \"proof_one_data\": \"4713734307765350282\",\n" +
   "                        \"proof_zero_challenge\": 50361,\n" +
   "                        \"proof_one_challenge\": 11059,\n" +
   "                        \"challenge\": 61420,\n" +
@@ -142,17 +143,17 @@ const json_string = "{\n" +
   "                    \"sequence_order\": 5,\n" +
   "                    \"description_hash\": 61706,\n" +
   "                    \"ciphertext\": {\n" +
-  "                        \"pad\": 8212879103348661976,\n" +
-  "                        \"data\": 17731780476479972322\n" +
+  "                        \"pad\": \"8212879103348661976\",\n" +
+  "                        \"data\": \"17731780476479972322\"\n" +
   "                    },\n" +
   "                    \"crypto_hash\": 55827,\n" +
   "                    \"is_placeholder_selection\": true,\n" +
   "                    \"nonce\": 44427,\n" +
   "                    \"proof\": {\n" +
-  "                        \"proof_zero_pad\": 16960465299284500181,\n" +
-  "                        \"proof_zero_data\": 14180973964672850931,\n" +
-  "                        \"proof_one_pad\": 14485228182275779767,\n" +
-  "                        \"proof_one_data\": 13079974115890844967,\n" +
+  "                        \"proof_zero_pad\": \"16960465299284500181\",\n" +
+  "                        \"proof_zero_data\": \"14180973964672850931\",\n" +
+  "                        \"proof_one_pad\": \"14485228182275779767\",\n" +
+  "                        \"proof_one_data\": \"13079974115890844967\",\n" +
   "                        \"proof_zero_challenge\": 20131,\n" +
   "                        \"proof_one_challenge\": 28676,\n" +
   "                        \"challenge\": 48807,\n" +
@@ -164,14 +165,14 @@ const json_string = "{\n" +
   "                }\n" +
   "            ],\n" +
   "            \"ciphertext_accumulation\": {\n" +
-  "                \"pad\": 2538487341775279615,\n" +
-  "                \"data\": 16332417304602615325\n" +
+  "                \"pad\": \"2538487341775279615\",\n" +
+  "                \"data\": \"16332417304602615325\"\n" +
   "            },\n" +
   "            \"crypto_hash\": 53689,\n" +
   "            \"nonce\": 34874,\n" +
   "            \"proof\": {\n" +
-  "                \"pad\": 15654954763222468051,\n" +
-  "                \"data\": 15815819070899634924,\n" +
+  "                \"pad\": \"15654954763222468051\",\n" +
+  "                \"data\": \"15815819070899634924\",\n" +
   "                \"challenge\": 29600,\n" +
   "                \"response\": 28284,\n" +
   "                \"constant\": 2,\n" +
@@ -186,11 +187,29 @@ const json_string = "{\n" +
   "}";
 
 export function from_file_to_class(): void{
-  const result = JSON.parse(json_string);
+  const result = JSON.parse(json_string, (key, value) => {
+    if (key === "pad") {
+      return BigInt(value);
+    } else if (key === "data") {
+      return BigInt(value);
+    } else if (key === "proof_zero_pad") {
+      return BigInt(value);
+    } else if (key === "proof_zero_data") {
+      return BigInt(value);
+    } else if (key === "proof_one_pad") {
+      return BigInt(value);
+    } else if (key === "proof_one_data") {
+      return BigInt(value);
+    }
+    else {
+      return value;
+    }
+  });
   console.log(result);
   console.log(result.contests[0].proof.pad);
-  const newEncryptedBallot = plainToClass(CiphertextBallot, result);
+  const newEncryptedBallot= plainToClass(CiphertextBallot, result as CiphertextBallot);
   console.log(newEncryptedBallot);
+  console.log(get_optional(newEncryptedBallot.contests[0].proof).pad);
 }
 
 // export function encrypt_compatible_testing_demo(): CiphertextBallot{
