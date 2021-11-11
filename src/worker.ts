d1ca6f9517ece5f47926e9468880b28f39cedb1c
Alex guo <alexiland@outlook.com>
2021-11-10 18:38:53 -0600

    check lint error

--- src/worker.ts
+++
@@ -1,7 +0,0 @@
-import {get_optional} from "./utils";
-import {encrypt_ballot_contest} from "./simple_elections";
-onmessage = function(e) {
-    console.log("*******")
-    const result = get_optional(encrypt_ballot_contest(e.data[0], e.data[1], e.data[2]));
-    postMessage(result);
-}
