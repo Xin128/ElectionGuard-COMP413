604584640795d54e9ed1ac16dc0ce0bd486008a2
Yanyu Zhong <yz133@rice.edu>
2021-11-22 12:26:20 -0600

    Deleted comments that are unused in simple_elections.ts

--- ElectionGuard-COMP413/src/serialization.test.ts
+++ ElectionGuard-COMP413/src/serialization.test.ts
@@ -56,7 +56,7 @@ describe("TestDeserialization", () => {
     if (!fs.existsSync(testFolder)){
       fs.mkdirSync(testFolder);
     }
-    
+
     fs.readdirSync(testFolder).forEach(file => {
       if (file !== testFolder) {
         const path2TestVector = testFolder + "\\" + file;
@@ -71,7 +71,7 @@ describe("TestDeserialization", () => {
           new ElementModQ(2),
           internal_manifest.manifest.crypto_hash(),
           undefined);
-          console.log("manifest hash in ts is ", internal_manifest.manifest.crypto_hash());
+//           console.log("manifest hash in ts is ", internal_manifest.manifest.crypto_hash());
           const encryption_seed = new ElementModQ(BigInt('88136692332113344175662474900446441286169260372780056734314948839391938984061'));
           const encrypted_ballot = get_optional(encrypt_ballot(input.plaintextBallot, internal_manifest, context, encryption_seed, get_optional(inputs.nonce)));
           expect(encrypted_ballot.crypto_hash.equals(input.output)).toBe(true);
