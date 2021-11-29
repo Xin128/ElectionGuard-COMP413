aafaecd11f30250e66ef138722efacacf0503d8d
Zihe Zhao <zz83@rice.edu>
2021-11-29 02:04:10 -0600

    tally front end

--- ElectionGuard-COMP413/demo/frontend/tally.js
+++ ElectionGuard-COMP413/demo/frontend/tally.js
@@ -1,2 +1,31 @@
-from 'tally.json' import *;
+var data;
 
+function readTextFile() {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "./tally.json", true);
+    rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4) {
+        var allText = rawFile.responseText;
+        document.getElementById("temp").innerHTML = allText;
+      }
+    }
+    rawFile.send();
+
+}
+
+readTextFile();
+
+document.getElementById("get").addEventListener("click", function() {
+    var data = JSON.parse(document.getElementById("temp").innerHTML);
+    document.getElementById("temp").innerHTML="";
+    for (var contest = 0; contest < 4; contest++){
+        var total = 0;
+        var postEls = document.getElementsByClassName('contest-'+contest),
+        postElsCount = postEls.length;
+        for (var i = 0; i < postElsCount; i++) {
+            document.getElementById(postEls[i].id).innerHTML = data[postEls[i].id];
+            total += parseInt(data[postEls[i].id]);
+        }
+        document.getElementById("total-"+contest).innerHTML = total;
+    }
+});
