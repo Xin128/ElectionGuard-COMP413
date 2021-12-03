bd48da97c91ba2f73cc561e3cb8d1010d017a706
Alex guo <alexiland@outlook.com>
2021-12-03 15:06:55 -0600

    changed frontend question 4

---
+++ dist/tsc/onePlusOne.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.plus = void 0;
+const plus = (a, b) => a + b;
+exports.plus = plus;
+//# sourceMappingURL=onePlusOne.js.map
